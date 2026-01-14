37ad899085fc722347552af78f0993a6de5677fe
Dhamu-code <dhamodharan130602@gmail.com>
2026-01-14 20:11:21 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port", PORT));
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"));
 
@@ -22,6 +25,4 @@ app.use("/api/payroll", payrollRoutes);
 app.use("/api/attendance", attendanceRoutes);
 
 
-app.listen(5000, () =>
-  console.log("Server running on port 5000")
-);
+
