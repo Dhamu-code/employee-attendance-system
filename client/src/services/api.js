6d37fd2c47a3ac18479bc3b7cedd85be3e92b2c5
Dhamu-code <dhamodharan130602@gmail.com>
2026-01-14 20:29:12 +0530

    Update API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://employee-attendance-system-wqyr.onrender.com/api",
 });
 
 API.interceptors.request.use(
